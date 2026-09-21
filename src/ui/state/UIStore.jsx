5df63a0dcb93bbce2bbd1f34675f1f38a234a133
rafaelvalquer <109303318+rafaelvalquer@users.noreply.github.com>
2026-09-21 19:19:35 -0300

    refactor(ui-state): consume testable reducer

--- src/ui/state/UIStore.jsx
+++ src/ui/state/UIStore.jsx
@@ -1,43 +1,10 @@
 import { createContext, useContext, useEffect, useMemo, useReducer } from "react";
-
-const initialState = {
-  activePanel: null,
-  simulationLabOpen: false,
-  overlayToolbarOpen: false,
-  minimapVisible: true,
-  mode: "GAME"
-};
-
-function reducer(state, action) {
-  switch (action.type) {
-    case "OPEN_PANEL":
-      return { ...state, activePanel: action.panel };
-    case "TOGGLE_PANEL":
-      return {
-        ...state,
-        activePanel: state.activePanel === action.panel ? null : action.panel
-      };
-    case "CLOSE_PANEL":
-      return { ...state, activePanel: null };
-    case "TOGGLE_LAB":
-      return { ...state, simulationLabOpen: !state.simulationLabOpen };
-    case "SET_LAB":
-      return { ...state, simulationLabOpen: Boolean(action.open) };
-    case "TOGGLE_OVERLAYS":
-      return { ...state, overlayToolbarOpen: !state.overlayToolbarOpen };
-    case "TOGGLE_MINIMAP":
-      return { ...state, minimapVisible: !state.minimapVisible };
-    case "SET_MODE":
-      return { ...state, mode: action.mode };
-    default:
-      return state;
-  }
-}
+import { initialUIState, uiReducer } from "./UIState.js";
 
 const UIContext = createContext(null);
 
 export function UIProvider({ children }) {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(uiReducer, initialUIState);
 
   useEffect(() => {
     const onKeyDown = (event) => {
